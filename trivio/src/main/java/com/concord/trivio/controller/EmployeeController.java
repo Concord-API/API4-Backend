62b1b479c93351e49c90cf53a9b1f50358a51b47
isaac <isaacoliveirazuza28@gmail.com>
2026-03-26 02:17:44 +0000

    API-22 1.1.2 feat: Criar API para cadastro de Employee

---
+++ trivio/src/main/java/com/concord/trivio/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.concord.trivio.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.concord.trivio.entity.Employee;
+import com.concord.trivio.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<Employee> cadastrar(@RequestBody Employee employee) {
+        Employee salvo = employeeService.cadastrar(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(salvo);
+    }
+
+}
