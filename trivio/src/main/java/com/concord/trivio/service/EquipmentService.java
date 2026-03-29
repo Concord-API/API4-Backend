7dcffd85f037efd028069d83f2ab64cbcd200993
JoaoBaranov <baranovjoao@outlook.com>
2026-03-29 21:10:45 +0000

    API-25 2.1.3 feat: Criar API de Alteração de Equipment

---
+++ trivio/src/main/java/com/concord/trivio/service/EquipmentService.java
@@ -0,0 +1,9 @@
+package com.concord.trivio.service;
+
+import com.concord.trivio.entity.Equipment;
+
+public interface EquipmentService {
+
+    public Equipment alterar(Long id, Equipment equipment);
+
+}
---
+++ trivio/src/main/java/com/concord/trivio/service/EquipmentServiceImpl.java
@@ -0,0 +1,29 @@
+package com.concord.trivio.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.concord.trivio.entity.Equipment;
+import com.concord.trivio.repository.EquipmentRepository;
+
+@Service
+public class EquipmentServiceImpl implements EquipmentService {
+
+    @Autowired
+    private EquipmentRepository equipmentRepository;
+
+    @Override
+    public Equipment alterar(Long id, Equipment equipment) {
+
+        Equipment existente = equipmentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Equipment não encontrado"));
+
+        existente.setName(equipment.getName());
+        existente.setModel(equipment.getModel());
+        existente.setManufacturer(equipment.getManufacturer());
+        existente.setActive(equipment.getActive());
+
+        return equipmentRepository.save(existente);
+    }
+
+}
