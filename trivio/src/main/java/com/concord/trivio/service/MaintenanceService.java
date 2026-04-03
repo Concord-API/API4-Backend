e6912cc0139b4e9166c8aa35b65192610a40d135
IsaacOliveiraSouza <isaacoliveirazuza28@gmail.com>
2026-04-03 20:58:04 +0000

    API-60 feat: 3.1.3 Criar API de Alteração de Maintenance

--- trivio/src/main/java/com/concord/trivio/service/MaintenanceService.java
+++ trivio/src/main/java/com/concord/trivio/service/MaintenanceService.java
@@ -6,4 +6,6 @@ import com.concord.trivio.dto.MaintenanceResponseDTO;
 public interface MaintenanceService {
 
     public MaintenanceResponseDTO cadastrar(MaintenanceRequest maintenanceRequest);
+
+    public MaintenanceResponseDTO atualizar(Long id, MaintenanceRequest maintenanceRequest);
 }
