f6667b5f9755611ec011ce0712492b524f43f77b
Victor Nogueira <vgdsno@gmail.com>
2026-04-04 18:57:08 -0300

    API-47 2.2.2 Criada API de alteração de Requisito

--- trivio/src/main/java/com/concord/trivio/service/RequirementService.java
+++ trivio/src/main/java/com/concord/trivio/service/RequirementService.java
@@ -6,4 +6,6 @@ import com.concord.trivio.entity.Requirement;
 public interface RequirementService {
 
     Requirement cadastrar(RequirementRequest request);
+
+    Requirement atualizar(Long id, RequirementRequest request);
 }
