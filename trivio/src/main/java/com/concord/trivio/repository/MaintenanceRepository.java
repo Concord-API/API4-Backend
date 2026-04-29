217b3c4b3b56b3c36b0e57af20a5716a95c222bf
vp-p <viniciuspenteadop@gmail.com>
2026-04-29 23:18:50 +0000

    API-88 6.4 feat: implementação da query para verificar se a manutenção anterior, referente a que vai ser criada, foi de fato concluída e se já possui uma manutenção posterior a ela

--- trivio/src/main/java/com/concord/trivio/repository/MaintenanceRepository.java
+++ trivio/src/main/java/com/concord/trivio/repository/MaintenanceRepository.java
@@ -1,4 +1,5 @@
 package com.concord.trivio.repository;
+import java.time.LocalDate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,5 +15,13 @@ public interface MaintenanceRepository extends JpaRepository<Maintenance, Long> 
     Optional<Maintenance> findByIdWithEmployees(@Param("id") Long id);
 
     @Query("SELECT DISTINCT m FROM Maintenance m LEFT JOIN FETCH m.employees me LEFT JOIN FETCH me.employee")
-    List<Maintenance> findAllWithEmployees();
+    List<Maintenance> findAllWithEmployees();  
+
+    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END " +
+       "FROM Maintenance m " +
+       "WHERE m.contract.id = :contractId AND m.date > :date AND m.active = true")
+    boolean existsNextMaintenance(@Param("contractId") Long contractId,
+                               @Param("date") LocalDate date);
 }
+
+
